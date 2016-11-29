f703d68c5cab860afdd35a3b0fb2fe9964e6882c
Roxane <roxane.richard@ymail.com>
2016-11-29 14:53:52 +0100

    DetailsPageController url jsp

--- Makaromi-war/src/java/controllers/sub/DetailsPageController.java
+++ Makaromi-war/src/java/controllers/sub/DetailsPageController.java
@@ -33,7 +33,7 @@ public class DetailsPageController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+            String url = "/WEB-INF/jsp/detailsPage.jsp";
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
